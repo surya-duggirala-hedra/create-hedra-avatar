c9b95760e972a06338d81f9b4ff589df088e25c2
Surya Duggirala <surya@hedra.com>
2025-06-23 17:43:17 -0700

    commit

--- template/backend/agent_worker.py
+++ template/backend/agent_worker.py
@@ -10,16 +10,17 @@ from livekit.plugins import hedra, openai
 logger = logging.getLogger("hedra-avatar-example")
 logger.setLevel(logging.INFO)
 
-load_dotenv()
+load_dotenv(".env.local")
 
 
 async def entrypoint(ctx: JobContext):
     session = AgentSession(
-        llm=openai.realtime.RealtimeModel(voice="alloy"),
+        # List of voices here: https://www.openai.fm/
+        llm=openai.realtime.RealtimeModel(voice="ash"),
     )
 
     # upload an avatar image or use an avatar id from hedra
-    avatar_image = Image.open(os.path.join(os.path.dirname(__file__), "avatar.jpg"))
+    avatar_image = Image.open(os.path.join(os.path.dirname(__file__), "assets/default_avatar.png"))
     hedra_avatar = hedra.AvatarSession(avatar_image=avatar_image)
     await hedra_avatar.start(session, room=ctx.room)
 
